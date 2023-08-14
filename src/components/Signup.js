69a931604435c03b13f686135ed067ffeacf913e
Nazim-1 <nazimpure@gmail.com>
2023-08-14 16:41:11 +0530

    3rd

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,63 +1,109 @@
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = (props) => {
-    const [credentials, setCredentials] = useState({name:"", email: "", password: "", cpassword: ""}) 
-    let history = useNavigate();
-    const host = "https://inotebook-kt05.onrender.com"
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  let history = useNavigate();
+  const host = "https://inotebook-server-nine.vercel.app";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const {name, email, password} = credentials;
-        const response = await fetch(`${host}/api/auth/createuser`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({name, email, password})
-        });
-        const json = await response.json()
-        console.log(json);
-        if(json.success){
-            // Save the auth token and redirect
-            localStorage.setItem('token', json.authtoken); 
-            history("/");
-            props.showAlert("Successfully Signup", "success")
-        }
-        else{
-           props.showAlert("Invalid credentials", "danger")
-        }
+    const { name, email, password } = credentials;
+    const response = await fetch(`${host}/api/auth/createuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem("token", json.authtoken);
+      history("/");
+      props.showAlert("Successfully Signup", "success");
+    } else {
+      props.showAlert("Invalid credentials", "danger");
     }
+  };
 
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-    }
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" value={credentials.name} onChange={onChange} id="name" name="name" aria-describedby="emailHelp" />
-
-                </div>
-                <div className="mb-3">
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
-
-                    </div>
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} name="password" id="password" minLength={5} required />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="cpassword" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={credentials.cpassword} onChange={onChange} name="cpassword" id="cpassword" minLength={5} required/>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            value={credentials.name}
+            onChange={onChange}
+            id="name"
+            name="name"
+            aria-describedby="emailHelp"
+          />
         </div>
-    )
-}
+        <div className="mb-3">
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              value={credentials.email}
+              onChange={onChange}
+              id="email"
+              name="email"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            value={credentials.password}
+            onChange={onChange}
+            name="password"
+            id="password"
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            value={credentials.cpassword}
+            onChange={onChange}
+            name="cpassword"
+            id="cpassword"
+            minLength={5}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
